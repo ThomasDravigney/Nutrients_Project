cac0bf990993dc768c0af7922edcaf6d1c957e22
ThomasDravigney <dravigney.thomas@gmail.com>
2021-10-18 17:36:02 +0200

    updated Search function

--- food_api.py
+++ food_api.py
@@ -4,6 +4,5 @@ import openfoodfacts
 def search(keywords):
     search_result = openfoodfacts.products.advanced_search({
      "search_terms": keywords,
-     "countries": "fr",
-     "page_size": "20"})
+     "page_size": "50"})
     return search_result
