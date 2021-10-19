1b84cfce8aaa9232c7af96b1ce41a1e95dd6f0d0
ThomasDravigney <dravigney.thomas@gmail.com>
2021-10-20 00:55:17 +0200

    updated SQLAlchemy URI

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
--- config.py
+++ config.py
@@ -4,6 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = 'postgresql://ThomasDravigney:Wzdtj17g@aabzi83kl4lqdn.caudcphqyozw.eu-west-3.rds.amazonaws.com:5432/postgres'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
